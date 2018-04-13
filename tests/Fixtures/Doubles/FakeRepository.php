63042d1ddf835f45e125bd0f02a1bae2affce03c
Shudd3r <q3.shudder@gmail.com>
2018-04-13 16:45:38 +0200

    Added tests for SessionAuthentication

---
+++ tests/Fixtures/Doubles/FakeRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of Polymorphine/User package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Polymorphine\User\Tests\Fixtures\Doubles;
+
+
+use Polymorphine\User\Entity\GuestUser;
+use Polymorphine\User\Repository;
+use Polymorphine\User\UserEntity;
+
+
+class FakeRepository implements Repository
+{
+    private $id;
+
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+    public function getUserById(string $id): UserEntity
+    {
+        return $this->id === $id ? new FakeUser($id) : $this->guestUser();
+    }
+
+    public function getUserByCookieToken(string $id): UserEntity
+    {
+        return $this->id === $id ? new FakeUser($id) : $this->guestUser();
+    }
+
+    public function guestUser(): UserEntity
+    {
+        return new GuestUser();
+    }
+}
---
+++ tests/Fixtures/Doubles/FakeUser.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of Polymorphine/User package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Polymorphine\User\Tests\Fixtures\Doubles;
+
+use Polymorphine\User\Entity\GuestUser;
+
+
+class FakeUser extends GuestUser
+{
+    private $id;
+
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+
+    public function id(): string
+    {
+        return $this->id;
+    }
+}
