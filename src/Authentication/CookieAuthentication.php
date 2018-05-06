ad233d4ae7bfd3b51a65b5fb92e68af95fb19414
Shudd3r <q3.shudder@gmail.com>
2018-05-06 13:49:58 +0200

    Fallback CookieAuthentication resumes session

--- src/Authentication/CookieAuthentication.php
+++ src/Authentication/CookieAuthentication.php
@@ -13,6 +13,7 @@ namespace Polymorphine\User\Authentication;
 
 use Polymorphine\User\Authentication;
 use Polymorphine\User\Cookies\ResponseCookies;
+use Polymorphine\User\Session\SessionDataStorage;
 use Polymorphine\User\Repository;
 use Polymorphine\User\UserEntity;
 
@@ -22,12 +23,14 @@ class CookieAuthentication implements Authentication
     protected const REMEMBER_COOKIE = 'remember';
 
     private $cookies;
+    private $session;
     private $repository;
     private $token;
 
-    public function __construct(ResponseCookies $cookies, Repository $repository)
+    public function __construct(ResponseCookies $cookies, SessionDataStorage $session, Repository $repository)
     {
         $this->cookies    = $cookies;
+        $this->session    = $session;
         $this->repository = $repository;
     }
 
@@ -38,13 +41,13 @@ class CookieAuthentication implements Authentication
 
     public function user(): UserEntity
     {
-        $user = ($this->token)
-            ? $this->repository->getUserByCookieToken($this->token)
-            : $this->repository->guestUser();
+        if (!$this->token) { return $this->repository->guestUser(); }
 
-        if (!$user->isLoggedIn() && $this->token) {
-            $this->cookies->cookie(self::REMEMBER_COOKIE)->remove();
-        }
+        $user = $this->repository->getUserByCookieToken($this->token);
+
+        $user->isLoggedIn()
+            ? $this->session->set($this->session::USER_ID_KEY, $user->id())
+            : $this->cookies->cookie(self::REMEMBER_COOKIE)->remove();
 
         return $user;
     }
